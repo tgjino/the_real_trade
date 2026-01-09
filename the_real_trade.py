8e200b9bddc108b2168f18fd1c877ec11a0ba154
tgjino <108174360+tgjino@users.noreply.github.com>
2026-01-09 15:35:19 +0530

    a initial code added

--- the_real_trade.py
+++ the_real_trade.py
@@ -1,1 +1,12 @@
- 
+import streamlit as st 
+st.title("The Real Trade")
+
+sell_premium = st.number_input("Sell Premium", value = 0.0)
+buy_premium = st.number_input("Buy Premium", value = 0.0)
+
+if st.buttom("Calculate Profit"):
+    if sell_premium > 0:
+        net_profit = (sell_premium - buy_premium) * 25
+        st.sucess(f"Yours approximate profit": {net_profit})
+    else:
+        st.warning("Give a amount")
