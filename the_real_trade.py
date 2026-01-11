38004c8b5f314ecabaa44faea7e401109bb6d319
tgjino <108174360+tgjino@users.noreply.github.com>
2026-01-11 08:20:00 +0530

    an error cleared

--- the_real_trade.py
+++ the_real_trade.py
@@ -4,7 +4,7 @@ st.title("The Real Trade")
 sell_premium = st.number_input("Sell Premium", value = 0.0)
 buy_premium = st.number_input("Buy Premium", value = 0.0)
 
-if st.buttom("Calculate Profit"):
+if st.button("Calculate Profit"):
     if sell_premium > 0:
         net_profit = (sell_premium - buy_premium) * 25
         st.sucess(f"Yours approximate profit: {net_profit}")
